1754c2081d51c9f5b6decaed58689e13a0f9ebd9
lipemat <mat@matlipe.com>
2026-01-07 23:39:35 -0500

    Include dependencies directly required for jest testing

--- jest/jest.config.ts
+++ jest/jest.config.ts
@@ -2,6 +2,8 @@ import jestConfig from '../config/jest.config.mjs';
 
 jestConfig.moduleNameMapper = {
 	...jestConfig.moduleNameMapper,
+
+
 	// A temporary workaround for the shared package being a symlink.
 	// @todo remove once the shared package is published.
 	'^@lipemat/js-boilerplate-shared$': '<rootDir>/../../js-boilerplate-shared/index.ts',
