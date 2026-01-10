382e236f8360c6a5fa3bd77f74b8461b60a96489
lipemat <mat@matlipe.com>
2026-01-10 11:57:36 -0500

    Expose getBrowsersList and ensureJSExtension helpers from the index

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
-export {getExtensionsConfig} from './helpers/config.js';
+export {getExtensionsConfig, getBrowsersList, ensureJSExtension} from './helpers/config.js';
 export {getPackageConfig, type PackageConfig, modifyPackageConfig} from './helpers/package-config.js';
