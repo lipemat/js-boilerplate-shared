83588a834757463c76a9434a3fc1bbdc7b4ce898
lipemat <mat@matlipe.com>
2026-01-12 10:22:34 -0500

    Kill index.ts and .js
    Causing multiple paths to use making it hard to override consisitenly during tests

--- index.ts
+++
@@ -1,2 +0,0 @@
-export {getExtensionsConfig, getBrowsersList, ensureJSExtension} from './helpers/config.js';
-export {getPackageConfig, type PackageConfig, modifyPackageConfig} from './helpers/package-config.js';
