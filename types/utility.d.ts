e95be4e9b7cf6ed75245de0156a19a822eb3b2be
lipemat <mat@matlipe.com>
2026-02-18 06:17:42 -0500

    Include Optional in TS utilities

--- types/utility.d.ts
+++ types/utility.d.ts
@@ -4,3 +4,10 @@
  * @example AtLeast<Post, 'author' | 'post_title'> - post_title and author will be required, the rest optional.
  */
 export type AtLeast<T, K extends keyof T> = Partial<T> & Required<Pick<T, K>>;
+
+/**
+ * Make specified properties on a type optional.
+ *
+ * @example `Optional<Post, 'post_title'|'author'>` - `post_title` and `author` will be optional.
+ */
+export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
